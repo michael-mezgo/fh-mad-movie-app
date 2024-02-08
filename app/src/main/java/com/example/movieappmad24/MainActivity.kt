6d2085689ff6cdc47de6ecd51524ccde76ae1342
leonardo1710 <leon.freudenthaler@fh-campuswien.ac.at>
2024-02-08 14:21:33 +0100

    in lecture steps finished

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -4,15 +4,45 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ModifierInfo
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.movieappmad24.models.Movie
+import com.example.movieappmad24.models.getMovies
 import com.example.movieappmad24.ui.theme.MovieAppMAD24Theme
 
 class MainActivity : ComponentActivity() {
@@ -25,11 +55,83 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Card {
-                        Image(painter = painterResource(id = R.drawable.movie_image), contentDescription = "placeholder_image")
-                    }
+                    MovieList(movies = getMovies())
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MovieList(movies: List<Movie> = getMovies()){
+    LazyColumn {
+        items(movies) { movie ->
+            MovieRow(movie)
+        }
+    }
+}
+
+
+@Composable
+fun MovieRow(movie: Movie){
+    var showDetails by remember {
+        mutableStateOf(false)
+    }
+
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .padding(5.dp),
+        shape = ShapeDefaults.Large,
+        elevation = CardDefaults.cardElevation(10.dp)
+    ) {
+        Column {
+            Box(
+                modifier = Modifier
+                    .height(150.dp)
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.movie_image),
+                    contentScale = ContentScale.Crop,
+                    contentDescription = "placeholder image")
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(10.dp),
+                    contentAlignment = Alignment.TopEnd
+                ){
+                    Icon(
+                        tint = MaterialTheme.colorScheme.secondary,
+                        imageVector = Icons.Default.FavoriteBorder,
+                        contentDescription = "Add to favorites")
+                }
+            }
+            
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(6.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = movie.title)
+                Icon(modifier = Modifier
+                    .clickable {
+                       showDetails = !showDetails
+                    },
+                    imageVector =
+                        if (showDetails) Icons.Filled.KeyboardArrowDown
+                        else Icons.Default.KeyboardArrowUp, contentDescription = "show more")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DefaultPreview(){
+    MovieAppMAD24Theme {
+       MovieList(movies = getMovies())
     }
 }
