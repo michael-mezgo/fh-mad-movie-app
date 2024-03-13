536d9565eecae63e1d6ca971466c18dfc6a3cc6d
michael-mezgo <58122000+michael-mezgo@users.noreply.github.com>
2024-03-13 16:58:20 +0100

    Get movie in detail screen (lecture 2024-03-13)

--- app/src/main/java/com/example/movieappmad24/screens/DetailScreen.kt
+++ app/src/main/java/com/example/movieappmad24/screens/DetailScreen.kt
@@ -1,10 +1,39 @@
 package com.example.movieappmad24.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import com.example.movieappmad24.models.getMovies
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DetailScreen(movieId : String, navController: NavController) {
-    Text(text = "Hello detail-screen $movieId")
+fun DetailScreen(movieId: String, navController: NavController) {
+    val movie = getMovies().find { it.id == movieId }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            CenterAlignedTopAppBar(title = {
+                if (movie != null) {
+                    Text(movie.title)
+                } else Text("Detail page")
+            })
+        },
+
+        content = { paddingValues ->
+            Box(
+                modifier = Modifier
+                    .padding(paddingValues)
+            ) {
+                Text("abc")
+            }
+        }
+    )
 }
