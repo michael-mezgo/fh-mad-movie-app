f7b6222a370ee6d531bab31c278d164cacaa211d
michael-mezgo <58122000+michael-mezgo@users.noreply.github.com>
2024-03-23 12:08:52 +0100

    Added Pictures in LazyRow in DetailScreen Code cleanup

--- app/src/main/java/com/example/movieappmad24/screens/DetailScreen.kt
+++ app/src/main/java/com/example/movieappmad24/screens/DetailScreen.kt
@@ -1,15 +1,29 @@
 package com.example.movieappmad24.screens
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import com.example.movieappmad24.R
 import com.example.movieappmad24.models.getMovies
 import com.example.movieappmad24.widgets.MovieCard
 
@@ -31,8 +45,40 @@ fun DetailScreen(movieId: String, navController: NavController) {
                 modifier = Modifier
                     .padding(paddingValues)
             ) {
-                MovieCard(movie = movie)
+                Column {
+                    Row {
+                        MovieCard(movie = movie)
+                    }
+                    LazyRow(
+                        Modifier
+                            .height(300.dp)
+                    ) {
+                        this.items(items = movie.images) { image ->
+                            MoviePicture(resourceLink = image, title = movie.title)
+                        }
+                    }
+                }
+
             }
         }
     )
+}
+
+
+@Composable
+fun MoviePicture(resourceLink: String, title: String) {
+    Card(
+        shape = RoundedCornerShape(size = 20.dp),
+        modifier = Modifier
+            .padding(all = 15.dp)
+    ) {
+        AsyncImage(
+            model = resourceLink,
+            contentScale = ContentScale.Crop,
+            placeholder = painterResource(id = R.drawable.movie_image),
+            contentDescription = "$title Image",
+            modifier = Modifier
+                .aspectRatio(ratio = 1f)
+        )
+    }
 }
--- app/src/main/java/com/example/movieappmad24/screens/HomeScreen.kt
+++ app/src/main/java/com/example/movieappmad24/screens/HomeScreen.kt
@@ -1,53 +1,30 @@
 package com.example.movieappmad24.screens
 
-import android.util.Log
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import coil.compose.AsyncImage
-import com.example.movieappmad24.R
 import com.example.movieappmad24.models.Movie
 import com.example.movieappmad24.models.getMovies
 import com.example.movieappmad24.ui.theme.MovieAppMAD24Theme
@@ -80,10 +57,10 @@ fun HomeScreen(navController: NavController) {
 
 @Composable
 fun CreateNavigationBar(items: List<NavItem>) {
-    if(items.isEmpty())
+    if (items.isEmpty())
         return
 
-    var selectedItem by remember { // purposely val (Line 106 will be uncommented when navigation is used)
+    var selectedItem by remember { // purposely val (Line 72 will be uncommented when navigation is used)
         mutableStateOf(items[0])
     }
 
@@ -107,12 +84,16 @@ fun CreateNavigationBar(items: List<NavItem>) {
 }
 
 @Composable
-fun MovieList(movies: List<Movie> = getMovies(), padding: PaddingValues, navController: NavController) {
+fun MovieList(
+    movies: List<Movie> = getMovies(),
+    padding: PaddingValues,
+    navController: NavController
+) {
     LazyColumn(
         modifier = Modifier.padding(padding)
     ) {
         items(movies) { movie ->
-            MovieCard(movie) {movieId -> navController.navigate("detailscreen/$movieId")}
+            MovieCard(movie) { movieId -> navController.navigate("detailscreen/$movieId") }
         }
     }
 }
